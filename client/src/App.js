edea282ed3a02667b896cca48b3c2667ede6ae89
Simone Brundu <simone.brundu@cern.ch>
2019-10-18 23:17:06 +0200

    Change app.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App() {
       <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Fibonacci calculator</h1>
         <Link to="/">Home</Link>
         <Link to="/otherpage">Other Page</Link>
       </header>
